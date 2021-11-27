5caa186f4559d8d1f629bbb966e0f3eec5902874
mudable <mudableshilpa@gmail.com>
2021-11-27 19:48:24 +0530

    Added UC2 to enter the details of contact

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	Contact contacts;
+
+	private void addContacts() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter The First Name  :- ");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter The Last Name :- ");
+		String lastName = scanner.nextLine();
+		System.out.println("Enter The Address :- ");
+		String address = scanner.nextLine();
+		System.out.println("Enter The City :- ");
+		String city = scanner.nextLine();
+		System.out.println("Enter The State :- ");
+		String state = scanner.nextLine();
+		System.out.println("Enter The Zip Code :- ");
+		int zipCode = scanner.nextInt();
+		System.out.println("Enter The Mobile Number :- ");
+		String number = scanner.next();
+		System.out.println("Enter The Email-id :- ");
+		String email = scanner.next();
+		contacts = new Contact(firstName, lastName, address, state, city, zipCode, number, email);
+		System.out.println("Contact Successfully Added");
+	}
+
+	@Override
+	public String toString() {
+		return contacts.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+		AddressBookMain addressBook = new AddressBookMain();
+		addressBook.addContacts();
+		System.out.println(addressBook.toString());
+	}
+}
